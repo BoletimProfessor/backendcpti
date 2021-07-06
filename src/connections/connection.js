1b1c7261a9e2d1f7a04bef627418544089f81dd2
Caio José Peterman Tesch <boletimonline.cpti21@gmail.com>
2021-07-05 23:21:56 -0300

    versão final

--- src/connections/connection.js
+++ src/connections/connection.js
@@ -6,6 +6,7 @@ const mysqlconnection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port:41890
 })
 
 //Conectando com db
